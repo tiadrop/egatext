df1c80df21099cf005eb4170f34354b208316879
Aleta <aletabix@pm.me>
2026-08-10 17:44:22 +0100

    add ega font

--- src/bmf.ts
+++ src/bmf.ts
@@ -1,32 +1,38 @@
 import { Pipe2D } from "@xtia/pipe2d";
 import { CrtFont } from "./types.js";
 
-const isBlock = (charCode: number) => {
+const isLineRight = (charCode: number) => {
     // 192 to 223
     return charCode > 191 && charCode < 224;
 }
 
-export function loadBmfFont(data: Uint8Array, addGap?: boolean): CrtFont
-export function loadBmfFont(url: string, addGap?: boolean): Promise<CrtFont>
-export function loadBmfFont(data: string | Uint8Array, addGap?: boolean): Promise<CrtFont> | CrtFont {
+/**
+ * Parses BMF font data to the `Pipe2D<number>[]` format used by EGAText.
+ * @param data 
+ * @param addColumn Add an extra column per EGA's standard (default: true)
+ */
+export function loadBmfFont(data: Uint8Array, addColumn?: boolean): CrtFont
+export function loadBmfFont(url: string, addColumn?: boolean): Promise<CrtFont>
+export function loadBmfFont(data: string | Uint8Array, addColumn: boolean = true): Promise<CrtFont> | CrtFont {
 
 	if (typeof data == "string") {
-		return 	fetch(data).then(r => r.status == 200 ? r.bytes() : null).then(bytes => {
+		return fetch(data).then(r => r.status == 200 ? r.bytes() : null).then(bytes => {
 			if (!bytes) throw new Error("Failed to fetch font data");
-			return loadBmfFont(bytes, addGap)
+			return loadBmfFont(bytes, addColumn)
 		});
 	}
 
-	if (!data || data.length != 4096) throw new Error("Failed to load font");
+	const rowCount = data.length / 256;
+	if (!Number.isInteger(rowCount)) throw new Error("Byte length must be a multiple of 256");
 	const bytes = [...data];
 
-	const rows = bytes.map(addGap
+	const rows = bytes.map(addColumn
 		? (b, i) => (
-				[b&128,b&64,b&32,b&16,b&8,b&4,b&2,b&1,isBlock(Math.floor(i / 16))?b&1:0]
+				[b&128,b&64,b&32,b&16,b&8,b&4,b&2,b&1,isLineRight(Math.floor(i / rowCount)) ? b&1 :0]
 			).map(n => n > 0 ? 1 : 0)
 		: b => [b&128,b&64,b&32,b&16,b&8,b&4,b&2,b&1].map(n => n > 0 ? 1 : 0));
 
-	return Array.from({ length: 256 }, (_, i) => Pipe2D.fromRows(rows.slice(i * 16, i * 16 + 16), addGap ? 9 : 8).stash());
+	return Array.from({ length: 256 }, (_, i) => Pipe2D.fromRows(rows.slice(i * rowCount, i * rowCount + rowCount), addColumn ? 9 : 8).stash());
 
 }
 
