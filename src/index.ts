fd00111ae73320770a2eadf8c3449a119504ceb8
Aleta <aletabix@pm.me>
2026-08-10 17:45:20 +0100

    add ega font

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export { loadBmfFont } from "./bmf.js";
 export { namedChars, accentedLetters, blockChars, lineSets } from "./charsets.js";
 export { CrtFont, LineSet, ForegroundColour, BackgroundColour, EGAData } from "./types.js";
 export { wideCharToByte437, byte437ToWideChar } from "./cp437.js";
-export { vgaFont } from "./vga-font.js"
+export { vgaFont8Col, vgaFont9Col, egaFont8Col, egaFont9Col } from "./fonts.js"
